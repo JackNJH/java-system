d1130fe9ed3b293e001ccbde138f7d135ac1e9c2
Shade <79087541+Shadewebdev@users.noreply.github.com>
2024-03-21 15:28:57 +0800

    Updated technician.java
    Updated constructors to allow for object creation without a set skill at the time of creation. Also created a class method to set the skill of the technician object.

--- src/utils/Technician.java
+++ src/utils/Technician.java
@@ -22,14 +22,24 @@ public class Technician extends User {
         setRole("Technician");
     }
     
+    public Technician(String username, String password) {
+        super(username, password);
+        setRole("Technician");
+    }
+    
+    public void setSkill(String skill) {
+        technicianSkill = skill;
+        // To-Do: Update Technicians.txt data Index [2]
+    }
+    
     public void assignedTask() {
         this.technicianAvailability = false;
-        // To-Do: Update Technicians.txt data.
+        // To-Do: Update Technicians.txt data. Index [3]
     }
     
     public void completedTask() {
         this.technicianAvailability = true;
-        // To-Do: Update Technicians.txt data.
+        // To-Do: Update Technicians.txt data. Index [3]
     }
     
     // To-Do: Implement a technician's availability list to sort through. The issue is knowing where to place the list. Maybe in the Users,java class?
